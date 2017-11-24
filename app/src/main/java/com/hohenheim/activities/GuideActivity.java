62769ff3d26f017b777ae1cb3384bb694d61390a
Hohenheim <chenzaocheng375@pingan.com.cn>
2017-11-24 17:50:08 +0800

    [资源]添加res管理

--- app/src/main/java/com/hohenheim/activities/GuideActivity.java
+++ app/src/main/java/com/hohenheim/activities/GuideActivity.java
@@ -13,8 +13,8 @@ public class GuideActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
-        Toolbar bar = (Toolbar)findViewById(R.id.toolBar);
-        bar.setTitle("IF");
-        bar.setLogo(R.drawable.if_logo);
+//        Toolbar bar = (Toolbar)findViewById(R.id.toolBar);
+//        bar.setTitle("IF");
+//        bar.setLogo(R.drawable.if_logo);
     }
 }
