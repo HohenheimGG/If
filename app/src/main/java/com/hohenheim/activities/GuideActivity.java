f77c9aacb13ce6a98c2ecaaff275feb3c68da8b3
Hohenheim <eph.oliver@gmail.com>
2017-11-26 23:56:48 +0800

    [一级页面]修改NavBar UI

--- app/src/main/java/com/hohenheim/activities/GuideActivity.java
+++ app/src/main/java/com/hohenheim/activities/GuideActivity.java
@@ -1,7 +1,12 @@
 package com.hohenheim.activities;
 
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
 import com.hohenheim.R;
+import com.hohenheim.view.IFToolBar;
 
 
 public class GuideActivity extends BaseActivity {
@@ -10,6 +15,14 @@ public class GuideActivity extends BaseActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
+    }
 
+    @Override
+    protected void initToolBar(Toolbar toolBar) {
+        super.initToolBar(toolBar);
+        IFToolBar bar = (IFToolBar)toolBar;
+        TextView tvLeft = (TextView)bar.findViewById(R.id.tv_title_back_text);
+        tvLeft.setVisibility(View.VISIBLE);
+        tvLeft.setText("IF");
     }
 }
