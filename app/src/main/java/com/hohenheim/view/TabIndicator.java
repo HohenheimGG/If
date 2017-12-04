b625df82e03a24f7d0541284630f69b99ef61bae
Hohenheim <chenzaocheng375@pingan.com.cn>
2017-12-04 14:13:18 +0800

    [View]完成TabIndicator

--- app/src/main/java/com/hohenheim/view/TabIndicator.java
+++ app/src/main/java/com/hohenheim/view/TabIndicator.java
@@ -8,6 +8,7 @@ import android.graphics.Path;
 import android.os.Looper;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 import com.hohenheim.R;
@@ -24,14 +25,8 @@ public class TabIndicator extends View {
     public static final int DIRECTION_LEFT = 0;
     public static final int DIRECTION_RIGHT = 1;
 
-    private static final int DEFAULT_ORIENTATION = 2;
-    private static final int LEFT_TO_RIGHT = 3;
-    private static final int RIGHT_TO_LEFT = 4;
-
     private int mDirection = DIRECTION_LEFT;
-    private int mOrientation = LEFT_TO_RIGHT;
-    private float mLastProgress = 0;
-    private float progress = 0;
+    private float mProgress = 0;
     private Paint mPaint;
     private int mWidth;
     private int mHeight;
@@ -52,81 +47,39 @@ public class TabIndicator extends View {
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaint.setStrokeWidth(1);
         mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setColor(mFocusColor);
         setBackgroundColor(mUnFocusColor);
     }
 
     @Override
     public void onDraw(Canvas canvas) {
-        //数据清零
-        if(progress == 0) {
-            mOrientation = DEFAULT_ORIENTATION;
-            mLastProgress = 0;
-            return;
-        }
-
         mWidth = getMeasuredWidth();
         mHeight = getMeasuredHeight();
 
-        //判断滚动方向
-        if(mOrientation == DEFAULT_ORIENTATION && mLastProgress == 0) {
-            mLastProgress = progress;
-        } else if(mOrientation == DEFAULT_ORIENTATION) {
-            if(mLastProgress > progress) {
-                mOrientation = RIGHT_TO_LEFT;
-            } else {
-                mOrientation = LEFT_TO_RIGHT;
+        if(mProgress >= 0.5) {
+            if(mProgress != 1) {
+                canvas.scale(1.5f, 1);
+                canvas.translate(-5, 0);
             }
-        }
-
-
-        if(mOrientation == LEFT_TO_RIGHT) {
-            if(mDirection == DIRECTION_LEFT) {
-                if(progress >= 0.5) {
-                    drawSizeScale(canvas);
-                } else {
-
-                }
-            } else if(mDirection == DIRECTION_RIGHT) {
-                if(progress >= 0.5) {
-
-                } else {
-                    drawSizeScale(canvas);
-
-                }
-            }
-        }
-
-        //线条粗细变化, 粗 -> 细 || 细 -> 粗
-        if(progress >= 0.5) {
-            drawSizeScale(canvas);
+            drawSizeScale(canvas, mProgress - 0.5f);
             return;
         }
 
         if(mDirection == DIRECTION_LEFT) {
-            if(mOrientation == LEFT_TO_RIGHT)
-                drawLeftLength(canvas);
-            else if(mOrientation == RIGHT_TO_LEFT)
-                drawRightLength(canvas);
-        } else if (mDirection == DIRECTION_RIGHT) {
-            if(mOrientation == RIGHT_TO_LEFT)
-                drawRightLength(canvas);
-            else if(mOrientation == LEFT_TO_RIGHT)
-                drawLeftLength(canvas);
+            drawLeftLength(canvas);
+        } else if(mDirection == DIRECTION_RIGHT) {
+            drawRightLength(canvas);
         }
 
-        //重置
-        if(progress == 1) {
-            mOrientation = DEFAULT_ORIENTATION;
-            mLastProgress = 0;
-        }
+        drawSizeScale(canvas, 0.5f - mProgress);
     }
 
     /**
      * 绘制线条粗细变化
      * @param canvas
      */
-    private void drawSizeScale(Canvas canvas) {
-        float fraction = (progress - 0.5f) / 0.5f;
+    private void drawSizeScale(Canvas canvas, float progress) {
+        float fraction = progress / 0.5f;
         int height = Math.max((int)(mHeight * fraction), mHeight / 2);
         int radius = height / 2;//半径
 
@@ -141,7 +94,6 @@ public class TabIndicator extends View {
                 0, mHeight / 2,
                 radius, mHeight / 2 - radius);//画圆
 
-        mPaint.setColor(mFocusColor);
         canvas.drawPath(path, mPaint);
     }
 
@@ -150,30 +102,11 @@ public class TabIndicator extends View {
      * @param canvas
      */
     private void drawLeftLength(Canvas canvas) {
-        float fraction = (0.5f - progress) / 0.5f;
+        float fraction = mProgress / 0.5f;
         int width = (int)(mWidth * fraction);
+        Log.d("TAG", "left mProgress: " + mProgress + " width: " + width + " mWidth: " + mWidth);
         canvas.translate(mWidth - width, 0);
 
-//        int radius = mHeight / 2;//半径
-//        float fraction = (0.5f - progress) / 0.5f;
-//
-//        int width = (int)(mWidth * fraction);
-//        Path path = new Path();
-//
-//        path.moveTo(radius + mWidth - width, mHeight / 2 - radius);
-//        path.lineTo(mWidth, mHeight / 2 - radius);
-//        path.lineTo(mWidth, mHeight / 2 + radius);
-//        path.lineTo(radius + mWidth - width, mHeight / 2 + radius);
-//        path.cubicTo(radius + mWidth - width, mHeight / 2 + radius,
-//                mWidth - width, mHeight / 2,
-//                radius + mWidth - width, mHeight / 2 - radius);
-//
-//        mPaint.setColor(mFocusColor);
-//        canvas.drawPath(path, mPaint);
-    }
-
-    private void drawRightScale(Canvas canvas) {
-        float fraction =
     }
 
     /**
@@ -181,25 +114,10 @@ public class TabIndicator extends View {
      * @param canvas
      */
     private void drawRightLength(Canvas canvas) {
-        float fraction = (0.5f - progress) / 0.5f;
+        float fraction = mProgress / 0.5f;
         int width = (int)(mWidth * fraction);
+        Log.d("TAG", "right mProgress: " + mProgress + " width: " + width + " mWidth: " + mWidth);
         canvas.translate(width - mWidth, 0);
-
-//        int radius = mHeight / 2;//半径
-//        float fraction = (0.5f - progress) / 0.5f;
-//        int width = (int)(mWidth * fraction);
-//        Path path = new Path();
-//
-//        path.moveTo(0, mHeight / 2 - radius);
-//        path.lineTo(width - radius, mHeight / 2 - radius);
-//        path.cubicTo(width - radius, mHeight / 2 - radius,
-//                width, mHeight / 2,
-//                width - radius, mHeight / 2 + radius);
-//        path.lineTo(0, mHeight / 2 + radius);
-//        path.lineTo(0, mHeight / 2 - radius);
-//
-//        mPaint.setColor(mFocusColor);
-//        canvas.drawPath(path, mPaint);
     }
 
     public void setDirection(int direction) {
@@ -207,7 +125,7 @@ public class TabIndicator extends View {
     }
 
     public void setProgress(float progress) {
-        this.progress = progress;
+        this.mProgress = progress;
         if(Looper.getMainLooper() == Looper.myLooper())
             invalidate();
         else
