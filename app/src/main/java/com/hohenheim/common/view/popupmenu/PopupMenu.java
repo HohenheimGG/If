5cb777e60d530afcc45b5c162704f913641654c2
Hohenheim <eph.oliver@gmail.com>
2017-12-15 10:56:22 +0800

    [View]把itemDecoration分离出来

--- app/src/main/java/com/hohenheim/common/view/popupmenu/PopupMenu.java
+++ app/src/main/java/com/hohenheim/common/view/popupmenu/PopupMenu.java
@@ -1,24 +1,13 @@
 package com.hohenheim.common.view.popupmenu;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.res.TypedArray;
-import android.graphics.Canvas;
-import android.graphics.Rect;
 import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
-import android.support.v4.view.ViewCompat;
-import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.PopupWindow;
-import android.widget.TextView;
-
 import com.hohenheim.R;
 
 import java.util.List;
@@ -59,7 +48,7 @@ public class PopupMenu {
     private void initView(Context context, List<String> menus, View parent) {
         RecyclerView rvView = (RecyclerView)parent.findViewById(R.id.menu_container);
         rvView.setLayoutManager(new GridLayoutManager(context, 1));
-        rvView.addItemDecoration(new DividerItemDecoration(context));
+        rvView.addItemDecoration(new MenuItemDecoration(context));
         rvView.setAdapter(new PopupMenuAdapter(menus));
     }
 
@@ -73,73 +62,5 @@ public class PopupMenu {
         popupWindow = null;
     }
 
-    private static class DividerItemDecoration extends RecyclerView.ItemDecoration {
-        private static final int[] ATTRS = new int[]{ android.R.attr.listDivider };
 
-        private Drawable mDivider;
-        private final Rect mBounds = new Rect();
-
-        /**
-         * Creates a divider {@link RecyclerView.ItemDecoration} that can be used with a
-         * {@link LinearLayoutManager}.
-         *
-         * @param context Current context, it will be used to access resources.
-         */
-        public DividerItemDecoration(Context context) {
-            final TypedArray a = context.obtainStyledAttributes(ATTRS);
-            mDivider = a.getDrawable(0);
-            a.recycle();
-        }
-
-
-        /**
-         * Sets the {@link Drawable} for this divider.
-         *
-         * @param drawable Drawable that should be used as a divider.
-         */
-        public void setDrawable(Drawable drawable) {
-            mDivider = drawable;
-        }
-
-        @Override
-        public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
-            if (parent.getLayoutManager() == null) {
-                return;
-            }
-            drawVertical(c, parent);
-        }
-
-        @SuppressLint("NewApi")
-        private void drawVertical(Canvas canvas, RecyclerView parent) {
-            canvas.save();
-            final int left;
-            final int right;
-            if (parent.getClipToPadding()) {
-                left = parent.getPaddingLeft()+40;
-                right = parent.getWidth() - parent.getPaddingRight()-40;
-                canvas.clipRect(left, parent.getPaddingTop(), right,
-                        parent.getHeight() - parent.getPaddingBottom());
-            } else {
-                left=40;
-                right = parent.getWidth()-40;
-            }
-
-            final int childCount = parent.getChildCount();
-            for (int i = 0; i < childCount-1; i++) {
-                final View child = parent.getChildAt(i);
-                parent.getDecoratedBoundsWithMargins(child, mBounds);
-                final int bottom = mBounds.bottom + Math.round(ViewCompat.getTranslationY(child));
-//            final int top = bottom - mDivider.getIntrinsicHeight();
-                final int top =bottom-2;
-                mDivider.setBounds(left, top, right, bottom);
-                mDivider.draw(canvas);
-            }
-            canvas.restore();
-        }
-        @Override
-        public void getItemOffsets(Rect outRect, View view, RecyclerView parent,
-                                   RecyclerView.State state) {
-            outRect.set(0, 0, 0, mDivider.getIntrinsicHeight());
-        }
-    }
 }
