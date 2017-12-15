2f05d68737c98d9b2cdffd8725a0ff6952285f89
Hohenheim <eph.oliver@gmail.com>
2017-12-15 10:52:40 +0800

    [View]完善popupmenu

--- app/src/main/java/com/hohenheim/common/view/popupmenu/PopupMenu.java
+++ app/src/main/java/com/hohenheim/common/view/popupmenu/PopupMenu.java
@@ -1,11 +1,13 @@
 package com.hohenheim.common.view.popupmenu;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
@@ -42,7 +44,7 @@ public class PopupMenu {
     public void show(View anchor, List<String> menus) {
         Context context = anchor.getContext();
         View view = LayoutInflater.from(context).inflate(R.layout.common_view_menu_container, null);
-        initView(context, menus);
+        initView(context, menus, view);
 
         popupWindow = new PopupWindow(view, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         popupWindow.setBackgroundDrawable(new ColorDrawable(context.getResources().getColor(R.color.white)));
@@ -54,10 +56,10 @@ public class PopupMenu {
         popupWindow.showAsDropDown(anchor);
     }
 
-    private void initView(Context context, List<String> menus) {
-        RecyclerView rvView = new RecyclerView(context);
+    private void initView(Context context, List<String> menus, View parent) {
+        RecyclerView rvView = (RecyclerView)parent.findViewById(R.id.menu_container);
         rvView.setLayoutManager(new GridLayoutManager(context, 1));
-        rvView.addItemDecoration(new DividerItemDecoration(context, DividerItemDecoration.VERTICAL));
+        rvView.addItemDecoration(new DividerItemDecoration(context));
         rvView.setAdapter(new PopupMenuAdapter(menus));
     }
 
@@ -69,5 +71,75 @@ public class PopupMenu {
         if(popupWindow != null)
             popupWindow.dismiss();
         popupWindow = null;
+    }
+
+    private static class DividerItemDecoration extends RecyclerView.ItemDecoration {
+        private static final int[] ATTRS = new int[]{ android.R.attr.listDivider };
+
+        private Drawable mDivider;
+        private final Rect mBounds = new Rect();
+
+        /**
+         * Creates a divider {@link RecyclerView.ItemDecoration} that can be used with a
+         * {@link LinearLayoutManager}.
+         *
+         * @param context Current context, it will be used to access resources.
+         */
+        public DividerItemDecoration(Context context) {
+            final TypedArray a = context.obtainStyledAttributes(ATTRS);
+            mDivider = a.getDrawable(0);
+            a.recycle();
+        }
+
+
+        /**
+         * Sets the {@link Drawable} for this divider.
+         *
+         * @param drawable Drawable that should be used as a divider.
+         */
+        public void setDrawable(Drawable drawable) {
+            mDivider = drawable;
+        }
+
+        @Override
+        public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+            if (parent.getLayoutManager() == null) {
+                return;
+            }
+            drawVertical(c, parent);
+        }
+
+        @SuppressLint("NewApi")
+        private void drawVertical(Canvas canvas, RecyclerView parent) {
+            canvas.save();
+            final int left;
+            final int right;
+            if (parent.getClipToPadding()) {
+                left = parent.getPaddingLeft()+40;
+                right = parent.getWidth() - parent.getPaddingRight()-40;
+                canvas.clipRect(left, parent.getPaddingTop(), right,
+                        parent.getHeight() - parent.getPaddingBottom());
+            } else {
+                left=40;
+                right = parent.getWidth()-40;
+            }
+
+            final int childCount = parent.getChildCount();
+            for (int i = 0; i < childCount-1; i++) {
+                final View child = parent.getChildAt(i);
+                parent.getDecoratedBoundsWithMargins(child, mBounds);
+                final int bottom = mBounds.bottom + Math.round(ViewCompat.getTranslationY(child));
+//            final int top = bottom - mDivider.getIntrinsicHeight();
+                final int top =bottom-2;
+                mDivider.setBounds(left, top, right, bottom);
+                mDivider.draw(canvas);
+            }
+            canvas.restore();
+        }
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent,
+                                   RecyclerView.State state) {
+            outRect.set(0, 0, 0, mDivider.getIntrinsicHeight());
+        }
     }
 }
